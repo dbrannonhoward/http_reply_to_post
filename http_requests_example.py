b5d95e5904927b1610389133a72f1133764e20b9
dbrannonhoward <dbrannonhoward@gmail.com>
2020-06-14 17:02:06 -0400

    variable refactor

--- http_requests_example.py
+++ http_requests_example.py
@@ -1,6 +1,6 @@
 #make a POST request
 import requests
 dictToSend = {'question': 'what is the answer?'}
-res = requests.post('http://localhost:5000/post/reply/misc/address', json=dictToSend)
-print('response from server:', res.text)
-dictFromServer = res.json()
+response_to_request = requests.post('http://localhost:5000/post/reply/misc/address', json=dictToSend)
+print('response from server:', response_to_request.text)
+dictFromServer = response_to_request.json()
